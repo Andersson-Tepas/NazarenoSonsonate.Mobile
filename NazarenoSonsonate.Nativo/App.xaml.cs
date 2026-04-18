b5f9d51355d9a4742760351ac6f8ffe9a86c5e05
Andersson-Tepas <ts24001@esfe.agape.edu.sv>
2026-04-18 09:52:16 -0600

    Corrección de configuración MAUI y soporte multiplataforma

---
+++ NazarenoSonsonate.Nativo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NazarenoSonsonate.Nativo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NazarenoSonsonate.Nativo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NazarenoSonsonate.Nativo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
